55c2ddb4a6330e5c21282ef620eea24da36484c9
HundredSpring <xuanbachvt1@gmail.com>
2022-03-29 17:23:13 +0700

    Bai Tap

---
+++ app/src/main/java/com/android/simplecalc/Calculator.java
@@ -0,0 +1,48 @@
+package com.android.simplecalc;
+
+public class Calculator {
+
+    // Available operations
+    public enum Operator {ADD, SUB, DIV, MUL, POW, GiaiThua}
+
+    /**
+     * Addition operation
+     */
+    public double add(double firstOperand, double secondOperand) {
+        return firstOperand + secondOperand;
+    }
+
+    /**
+     * Subtract operation
+     */
+    public double sub(double firstOperand, double secondOperand) {
+        return firstOperand - secondOperand;
+    }
+
+    /**
+     * Divide operation
+     */
+    public double div(double firstOperand, double secondOperand) {
+        return firstOperand / secondOperand;
+    }
+
+    /**
+     * Multiply operation
+     */
+    public double mul(double firstOperand, double secondOperand) {
+        return firstOperand * secondOperand;
+    }
+
+
+    public double pow(double firstOperand, double secondOperand){
+        return Math.pow(firstOperand, secondOperand);
+    }
+
+    public double tinhGiaithua(double firstOperand) {
+        if (firstOperand > 0) {
+            return firstOperand * tinhGiaithua(firstOperand - 1);
+        } else {
+            return 1;
+        }
+    }
+}
